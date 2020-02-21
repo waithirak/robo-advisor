77149fc55eb954db69e34f639f0c160b4a5ecfa1
waithirak <60021314+waithirak@users.noreply.github.com>
2020-02-20 20:33:33 -0500

    Parsed Response

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,42 +1,61 @@
 # app/robo_advisor.py
 
 import requests
-import json
-import os
-from dotenv import load_dotenv
+import json #included in the python language so we don't have to install anything
+#import os
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
-#API_KEY = os.environ["ALPHAVANTAGE_API_KEY"]
-#API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
+#API_KEY = os.environ["ALPHAVANTAGE_API_KEY"] - not going to use 
+#API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY") - not going to use 
+#API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS") - going to use
 
-symbol = "TSLA" # todo: ask for a user input
+#symbol = "TSLA" # todo: ask for a user input
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
-print("URL:", request_url)
+#request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+#print("URL:", request_url)
 
-print("REQUESTING SOME DATA FROM THE INTERNET...")
-response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(type(response.text)) #> str
+#print("REQUESTING SOME DATA FROM THE INTERNET...")
+#response = requests.get(request_url)
+#print(type(response))
+#print(response.status_code)
+#print(type(response.text)) #> str
 
 # handle response errors:
-if "Error Message" in response.text:
-    print("OOPS couldn't find that symbol, please try again")
-    exit()
+#if "Error Message" in response.text:
+    #print("OOPS couldn't find that symbol, please try again")
+    #exit()
 
-parsed_response = json.loads(response.text)
-print(type(parsed_response)) #> dict
+#parsed_response = json.loads(response.text)
+#print(type(parsed_response)) #> dict
 
-print(parsed_response)
+#print(parsed_response)
 
 #breakpoint()
+#End of inclass
+#2/20 going to work below here: 
+
+#
+# Info Inputs
+#
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+
+response = requests.get(request_url)
+#print(type(response))
+#print(response.status_code) #>200 which means it was successfull
+#print(response.text) # response text is a str so we need to use the JSON module to process it into a dictionary, what we want to do is parse the response from a string into a dictionary 
+
+parsed_response = json.loads(response.text)
+
+
+
+breakpoint()
 
 
 
 
+#Info Outputs
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
