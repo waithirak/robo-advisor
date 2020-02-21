df5df36b12e02d226b76a3624351d1867f821d17
waithirak <60021314+waithirak@users.noreply.github.com>
2020-02-20 19:01:56 -0500

    In Class 2/19

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # robo-advisor
+
+Project description: https://github.com/prof-rossetti/intro-to-python/blob/master/projects/robo-advisor/README.md
+
+## Environment Setup
+
+Create and activate a new Anaconda virtual environment:
+
+```sh
+conda create -n stocks-env python=3.7 # (first time only)
+conda activate stocks-env
+```
+
+From within the virtual environment, install the required packages specified in the "requirements.txt" file you created:
+
+```sh
+pip install -r requirements.txt
+```
+
+## API Setup
+
+Visit https://www.alphavantage.co/support/#api-key and obtain an API Key.
+
+
+Create a new file in this repo called .env and place inside the following contents:
+
+```
+ALPHAVANTAGE_API_KEY="___________"
+```
+
+## Usage
+
+From within the virtual environment, demonstrate your ability to run the Python script from the command-line:
+
+```sh
+python app/robo_advisor.py
+```
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,43 @@
 # app/robo_advisor.py
 
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#API_KEY = os.environ["ALPHAVANTAGE_API_KEY"]
+#API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY")
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
+
+symbol = "TSLA" # todo: ask for a user input
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+print("URL:", request_url)
+
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+response = requests.get(request_url)
+print(type(response))
+print(response.status_code)
+print(type(response.text)) #> str
+
+# handle response errors:
+if "Error Message" in response.text:
+    print("OOPS couldn't find that symbol, please try again")
+    exit()
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response)) #> dict
+
+print(parsed_response)
+
+#breakpoint()
+
+
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -16,3 +54,6 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+#in class 2/19
+#selected_stock = input("Please Input Selected Stock Symbol")
